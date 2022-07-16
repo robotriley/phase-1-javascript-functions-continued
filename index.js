8a9f6d3563580518b1d94e60a42747bbfa9a0240
riley soloner <rileysoloner@gmail.com>
2022-07-16 19:35:03 -0400

    commit tree 7de9d951d05b293c5722fc15329da2859b12faf1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+let saturdayFun = (activity = 'roller-skate') => {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+let mondayWork = (activity = 'go to the office') => {
+    return (`This Monday, I will ${activity}.`)
+}
+
+let wrapAdjective = (flair = '*') => {
+    let encouragingPromptFunction = (adj = "special") => {
+        return `You are ${flair}${adj}${flair}!`
+    }
+    return encouragingPromptFunction
+}
